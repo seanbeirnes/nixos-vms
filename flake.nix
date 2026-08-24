c4aff03da5fd139840b1c25b5b0aa92dcc10ca79
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2026-08-23 22:43:09 -0400

    feat: add bun and update NixOS version (#5)
    * feat: add bun
    
    * feat: add psql, python3, uv
    
    * fix: home manager replacing nvim init.lua

--- flake.nix
+++ flake.nix
@@ -2,18 +2,20 @@
   description = "NixOS VM configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs =
+    { nixpkgs, home-manager, ... }:
     let
       pkgs = nixpkgs.legacyPackages.aarch64-linux;
-    in {
+    in
+    {
       nixosConfigurations.dev-vm = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         specialArgs = {
@@ -27,16 +29,18 @@
 
       apps.aarch64-linux.apply-dev-vm = {
         type = "app";
-        program = "${pkgs.writeShellApplication {
-          name = "apply-dev-vm";
-          runtimeInputs = with pkgs; [
-            coreutils
-            diffutils
-            gnutar
-            nixos-rebuild
-          ];
-          text = builtins.readFile ./bin/apply-dev-vm;
-        }}/bin/apply-dev-vm";
+        program = "${
+          pkgs.writeShellApplication {
+            name = "apply-dev-vm";
+            runtimeInputs = with pkgs; [
+              coreutils
+              diffutils
+              gnutar
+              nixos-rebuild
+            ];
+            text = builtins.readFile ./bin/apply-dev-vm;
+          }
+        }/bin/apply-dev-vm";
       };
 
       formatter.aarch64-linux = pkgs.nixfmt;
--- hosts/dev-vm/default.nix
+++ hosts/dev-vm/default.nix
@@ -56,6 +56,7 @@
 
   environment.systemPackages = with pkgs; [
     bat
+    bun
     chromium
     eza
     fd
@@ -75,10 +76,13 @@
     nodejs
     opencode
     pnpm
+    postgresql_18
     process-compose
+    python3
     ripgrep
     tmux
     tree-sitter
+    uv
     vscode
     zoxide
   ];
